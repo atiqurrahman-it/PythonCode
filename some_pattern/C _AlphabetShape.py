5e6c29e49ea1ca4cb7738182a792b8979fa3d0aa
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-03-22 15:52:45 +0600

    C_AlphabetShape

---
+++ some_pattern/C _AlphabetShape.py
@@ -0,0 +1,19 @@
+#
+#   *  *  *  *
+# *
+# *
+# *
+# *
+# *
+#   *  *  *  *
+
+print("C_AlphabetShape\n")
+for row in range(7):
+    for col in range(5):
+        if col == 0 or (row == 0 or row == 6):
+            if col == 0 and (row == 0 or row == 6):
+                print(" ", end="")
+            else:
+                print("* ", end="")
+        print(" ", end="")
+    print()
