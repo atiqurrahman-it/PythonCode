cb5b03ef330f6681a8a651ef17f800de3809e4e6
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-03-22 15:35:52 +0600

    B _AlphabetShape

--- some_pattern/Right Triangle.py
+++ some_pattern/Right Triangle.py
@@ -1,4 +1,3 @@
-
 # Enter the any positive number : 5
 # Right trangle part 1
 # * 
@@ -21,34 +20,29 @@
 # * * * 
 # * * * * * 
 
-n=int(input("Enter the any positive number : "))
+n = int(input("Enter the any positive number : "))
 print("Right trangle part 1")
 
 # first way
-for i in range(1,n+1):
+for i in range(1, n + 1):
     for j in range(i):
-        print("* ",end="")
+        print("* ", end="")
     print()
 # second way
 print("Second way !.....")
-for i in range(1,n+1):
-    print("* "*i)
-   
-   
-   
-    
+for i in range(1, n + 1):
+    print("* " * i)
+
 print("Right trangle part 2")
 # second way
-for i in range(1,n+1):
-    if i%2!=0:
-        print("* "*i)
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        print("* " * i)
 # second way
 print("Second way !.....")
-for i in range(1,n+1):
-    if i%2!=0:
+for i in range(1, n + 1):
+    if i % 2 != 0:
         for j in range(i):
-            print("* ",end="")
-        
+            print("* ", end="")
+
         print()
-
-
--- some_pattern/Right_Triangle_NumberPattern.py
+++ some_pattern/Right_Triangle_NumberPattern.py
@@ -6,10 +6,9 @@
 # 1  2  3  4  5 
 
 
+n = int(input("Enter the any positive number : "))
 
-n=int(input("Enter the any positive number : "))
-
-for i in range(1,n+1,1):
-    for j in range(1,i+1):
-        print(j," ",end="")
+for i in range(1, n + 1, 1):
+    for j in range(1, i + 1):
+        print(j, " ", end="")
     print()
