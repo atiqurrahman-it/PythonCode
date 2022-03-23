3fd45654f9de5b7597a86a70a9da12241e0da40d
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-03-24 00:15:02 +0600

    update

--- some_pattern/I_AlphabetShape.py
+++ some_pattern/I_AlphabetShape.py
@@ -1,7 +1,7 @@
-print("H_AlphabetShape\n")
+print("I_AlphabetShape\n")
 for row in range(0, 6):
-    for col in range(0, 5):
-        if col == 2 or row == 0:
+    for col in range(0, 3):
+        if col == 1 or row == 0 or row==5:
             print("* ", end="")
         else:
             print("  ", end="")
