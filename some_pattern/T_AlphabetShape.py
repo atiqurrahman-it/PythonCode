d4cda9c199d04a2a48935ae0d6a6f29ef2966794
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2022-03-23 14:36:58 +0600

    R,S,T_AlphabetShape

---
+++ some_pattern/T_AlphabetShape.py
@@ -0,0 +1,15 @@
+# * * * * *
+#     *
+#     *
+#     *
+#     *
+#     *
+
+print("R_AlphabetShape.py")
+for row in range(0, 6):
+    for col in range(0, 5):
+        if row==0 or col==2:
+            print('* ',end='')
+        else:
+            print("  ",end="")
+    print()
